3825d99876130cb9df274ad1b7484f8c9108b70a
Haowen Liu <hliu746@bloomberg.net>
2024-03-06 10:21:35 -0500

    main: parse command line arguments with cxxopts

--- src/main.cpp
+++ src/main.cpp
@@ -4,60 +4,81 @@
 #include "cps-config-config.hpp"
 #include "printer.hpp"
 #include "search.hpp"
+#include <cxxopts.hpp>
 #include <fmt/format.h>
 
 int main(int argc, char * argv[]) {
-    if (argc < 2) {
-        fmt::println(stderr, "Error: Got {} arguments, expected at least 2", argc);
-        return 1;
-    }
-
     printer::Config conf{};
     std::vector<std::string> components;
     std::string format{"pkgconf"};
+    std::string package_name;
 
-    for (int i = 2; i < argc; ++i) {
-        const std::string_view arg = argv[i];
-        if (arg == "--cflags") {
-            conf.cflags = true;
-            conf.defines = true;
-            conf.includes = true;
-        } else if (arg == "--cflags-only-other") {
-            conf.cflags = true;
-            conf.defines = true;
-        } else if (arg == "--cflags-only-I") {
-            conf.includes = true;
-        } else if (arg == "--libs") {
-            conf.libs_link = true;
-            conf.libs_search = true;
-            conf.libs_other = true;
-        } else if (arg == "--libs-only-l") {
-            conf.libs_link = true;
-        } else if (arg == "--libs-only-L") {
-            conf.libs_search = true;
-        } else if (arg == "--libs-only-other") {
-            conf.libs_other = true;
-        } else if (arg == "--modversion") {
-            conf.mod_version = true;
-        } else if (arg == "--component") {
-            // TODO: error handling
-            components.push_back(argv[++i]);
-        } else if (arg.find("--component=") != std::string::npos) {
-            components.emplace_back(arg.substr(0, 13));
-        } else if (arg == "--version") {
-            fmt::println(VERSION);
-            return 0;
-        } else if (arg == "--format") {
-            format = argv[++i];
-        } else if (arg.substr(0, 10) == "--format=") {
-            format = arg.substr(10, arg.length());
-        } else if (arg.substr(0, 9) != "--format=") {
-            fmt::println(stderr, "Unknown command like argument {}", arg);
-            return 1;
-        }
+    cxxopts::Options options("cps-config");
+    // clang-format off
+    options.add_options()
+        ("package", "search for the specified package", cxxopts::value<std::string>())
+        ("cflags", "output all pre-processor and compiler flags")
+        ("cflags-only-I", "output -I flags")
+        ("cflags-only-other", "output cflags not covered by the cflags-only-I option")
+        ("libs", "output all linker flags")("libs-only-L", "print required LDPATH linker flags to stdout")
+        ("libs-only-l", "print required LIBNAME linker flags to stdout")
+        ("libs-only-other", "print required other linker flags to stdout")
+        ("modversion", "print the specified module's version to stdout")
+        ("component", "look for the specified component", cxxopts::value<std::vector<std::string>>())
+        ("version", "print cps-config version")
+        ("format", "output format", cxxopts::value<std::string>());
+    // clang-format on
+    options.parse_positional({"package"});
+    auto parsed_options = options.parse(argc, argv);
+
+    if (parsed_options.count("package")) {
+        package_name = parsed_options["package"].as<std::string>();
+    } else {
+        fmt::println(stderr, "Expected a package name to be specified");
+        return 1;
     }
 
-    auto && p = search::find_package(argv[1], components, components.empty());
+    if (parsed_options.count("cflags")) {
+        conf.cflags = true;
+        conf.defines = true;
+        conf.includes = true;
+    }
+    if (parsed_options.count("cflags-only-other")) {
+        conf.cflags = true;
+        conf.defines = true;
+    }
+    if (parsed_options.count("cflags-only-I")) {
+        conf.includes = true;
+    }
+    if (parsed_options.count("libs")) {
+        conf.libs_link = true;
+        conf.libs_search = true;
+        conf.libs_other = true;
+    }
+    if (parsed_options.count("libs-only-l")) {
+        conf.libs_link = true;
+    }
+    if (parsed_options.count("libs-only-L")) {
+        conf.libs_search = true;
+    }
+    if (parsed_options.count("libs-only-other")) {
+        conf.libs_other = true;
+    }
+    if (parsed_options.count("modversion")) {
+        conf.mod_version = true;
+    }
+    if (parsed_options.count("component")) {
+        components = parsed_options["component"].as<std::vector<std::string>>();
+    }
+    if (parsed_options.count("version")) {
+        fmt::println(VERSION);
+        return 0;
+    }
+    if (parsed_options.count("format")) {
+        format = parsed_options["format"].as<std::string>();
+    }
+
+    auto && p = search::find_package(package_name, components, components.empty());
     if (!p) {
         fmt::println(p.error());
         return 1;
