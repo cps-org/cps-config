290df080e9e0c960fc76ba3cff4778746ac5fa51
Tyler Weaver <maybe@tylerjw.dev>
2024-03-18 09:02:37 -0600

    Basic type tests

--- tests/loader.cpp
+++ tests/loader.cpp
@@ -40,5 +40,124 @@ namespace cps::utils::test {
                 << "should not have parsed, archive component requires `location` attribute";
         }
 
+        TEST(Loader, missing_name) {
+            std::stringstream ss(R"({
+    "cps_version": "0.10.0",
+    "components": {
+        "default": {
+            "type": "archive",
+            "location": "/",
+        }
+    }
+}
+)"s);
+            auto const package = cps::loader::load(ss, "missing_name");
+            ASSERT_FALSE(package.has_value()) << "should not have parsed, root requires `name` attribute";
+        }
+
+        TEST(Loader, missing_cps_version) {
+            std::stringstream ss(R"({
+    "name": "missing_cps_version",
+    "components": {
+        "default": {
+            "type": "archive",
+            "location": "/",
+        }
+    }
+}
+)"s);
+            auto const package = cps::loader::load(ss, "missing_cps_version");
+            ASSERT_FALSE(package.has_value()) << "should not have parsed, root requires `cps_version` attribute";
+        }
+
+        TEST(Loader, missing_components) {
+            std::stringstream ss(R"({
+    "name": "missing_components",
+    "cps_version": "0.10.0",
+}
+)"s);
+            auto const package = cps::loader::load(ss, "missing_components");
+            ASSERT_FALSE(package.has_value()) << "should not have parsed, root requires `components` attribute";
+        }
+
+        TEST(Loader, name_is_string) {
+            std::stringstream ss(R"({
+    "name": [],
+    "cps_version": "0.10.0",
+    "components": {
+        "default": {
+            "type": "archive",
+            "location": "/",
+        }
+    }
+}
+)"s);
+            auto const package = cps::loader::load(ss, "name_is_string");
+            ASSERT_FALSE(package.has_value()) << "should not have parsed, root requires `name` value to be string";
+        }
+
+        TEST(Loader, cps_version_is_string) {
+            std::stringstream ss(R"({
+    "name": "cps_version_is_string",
+    "cps_version": [],
+    "components": {
+        "default": {
+            "type": "archive",
+            "location": "/",
+        }
+    }
+}
+)"s);
+            auto const package = cps::loader::load(ss, "cps_version_is_string");
+            ASSERT_FALSE(package.has_value())
+                << "should not have parsed, root requires `cps_version` value to be string";
+        }
+
+        TEST(Loader, components_is_object) {
+            std::stringstream ss(R"({
+    "name": "components_is_object",
+    "cps_version": "0.10.0",
+    "components": [],
+}
+)"s);
+            auto const package = cps::loader::load(ss, "components_is_object");
+            ASSERT_FALSE(package.has_value())
+                << "should not have parsed, root requires `components` value to be object";
+        }
+
+        TEST(Loader, component_type_is_string) {
+            std::stringstream ss(R"({
+    "name": "component_type_is_string",
+    "cps_version": [],
+    "components": {
+        "default": {
+            "type": [],
+            "location": "/",
+        }
+    }
+}
+)"s);
+            auto const package = cps::loader::load(ss, "component_type_is_string");
+            ASSERT_FALSE(package.has_value())
+                << "should not have parsed, root requires `components.<name>.type` value to be string";
+        }
+
+        TEST(Loader, component_location_is_string) {
+            std::stringstream ss(R"({
+    "name": "component_location_is_string",
+    "cps_version": [],
+    "components": {
+        "default": {
+            "type": "a",
+            "location": [],
+        }
+    }
+}
+)"s);
+            auto const package = cps::loader::load(ss, "component_location_is_string");
+            ASSERT_FALSE(package.has_value())
+                << "should not have parsed, root requires `components.<name>.location` value to be string";
+        }
+
     } // unnamed namespace
 } // namespace cps::utils::test
