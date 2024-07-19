f0f75eec273aa3b17565712c62d2c4af515e1288
Dylan Baker <dylan@pnwbakers.com>
2024-07-19 13:35:38 -0700

    Handle CPS_PATH and CPS_PREFIX_PATH according to the current spec
    Where CPS_PATH behaves more like PKG_CONFIG_PATH (which is what we were
    already doing, incorrectly), and CPS_PREFIX_PATH has been added, and
    acts more like CMAKE_PREFIX_PATH

--- src/cps/env.hpp
+++ src/cps/env.hpp
@@ -11,6 +11,7 @@ namespace cps {
 
     struct Env {
         std::optional<std::string> cps_path = std::nullopt;
+        std::optional<std::string> cps_prefix_path = std::nullopt;
         bool debug_spew = false;
     };
 
