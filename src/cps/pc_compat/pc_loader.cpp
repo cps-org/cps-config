a857301f6332247f364131eeead36492623f7d36
Haowen Liu <lhw@lunacd.com>
2024-10-04 09:13:30 -0400

    fixup! Add initial scanner and parser for pkg-config file

--- src/cps/pc_compat/pc_loader.cpp
+++ src/cps/pc_compat/pc_loader.cpp
@@ -84,6 +84,7 @@ namespace cps::pc_compat {
         scan_begin(istream);
         yy::parser parse(*this);
         // To debug parser, uncomment the following line
+        // TODO: add a way to enable debug output without rebuilding
         // parse.set_debug_level(true);
         if (const int result = parse(); result != 0) {
             throw std::runtime_error("Failed to parse the given pkg-config file.");
